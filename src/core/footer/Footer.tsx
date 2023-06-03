e1b2fc4c535db3866c39811524fd8ab64fa4c69c
Kavindya Ranaweera <ranaweerakdilshani@gmail.com>
2023-06-03 12:26:17 +0530

    Changes for footer

--- src/core/footer/Footer.tsx
+++ src/core/footer/Footer.tsx
@@ -2,6 +2,10 @@ import './Footer.css';
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
     )
 }
